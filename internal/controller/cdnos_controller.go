e6621d94ccc136c8bf6859e3242e640528bb50e4
Noam Hadar <nhadar@drivenets.com>
2023-11-23 14:55:06 +0200

    add service reconcile func

--- internal/controller/cdnos_controller.go
+++ internal/controller/cdnos_controller.go
@@ -18,9 +18,15 @@ package controller
 
 import (
 	"context"
+	"fmt"
 
 	cdnosv1 "github.com/drivenets/cdnos-controller/api/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -55,14 +61,70 @@ func (r *CdnosReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
+	if err := r.reconcileService(ctx, cdnos); err != nil {
+		log.Error(err, "unable to get reconcile service: %v")
+		return ctrl.Result{}, err
+	}
+
 	log.Info("Cdnos reconciled", "Name", cdnos.Name, "Image", cdnos.Spec.Image, "Namespace", cdnos.Namespace)
 
 	return ctrl.Result{}, nil
+}
+
+func (r *CdnosReconciler) reconcileService(ctx context.Context, cdnos *cdnosv1.Cdnos) error {
+	var service corev1.Service
+	svcName := fmt.Sprintf("service-%s", cdnos.Name)
+
+	err := r.Get(ctx, types.NamespacedName{Name: svcName, Namespace: cdnos.Namespace}, &service)
+	var newService bool
+	if apierrors.IsNotFound(err) {
+		service.ObjectMeta = metav1.ObjectMeta{
+			Name:      svcName,
+			Namespace: cdnos.Namespace,
+			Labels: map[string]string{
+				"name": cdnos.Name,
+			},
+		}
+		service.Spec = corev1.ServiceSpec{
+			Type: corev1.ServiceTypeLoadBalancer,
+			Selector: map[string]string{
+				"app":  cdnos.Name,
+				"topo": cdnos.Namespace,
+			},
+		}
+		if err := ctrl.SetControllerReference(cdnos, &service, r.Scheme); err != nil {
+			return err
+		}
+		newService = true
+	} else if err != nil {
+		return err
+	}
+	service.Spec.Ports = []corev1.ServicePort{}
+	for name, p := range cdnos.Spec.Ports {
+		service.Spec.Ports = append(service.Spec.Ports, corev1.ServicePort{
+			Name:       name,
+			Port:       p.OuterPort,
+			Protocol:   corev1.ProtocolTCP,
+			TargetPort: intstr.FromInt(int(p.InnerPort)),
+		})
+	}
+	if len(cdnos.Spec.Ports) == 0 && newService {
+		return nil
+	}
+	if len(cdnos.Spec.Ports) == 0 {
+		return r.Delete(ctx, &service)
+	}
+	if newService {
+		return r.Create(ctx, &service)
+	}
+
+	return r.Update(ctx, &service)
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *CdnosReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&cdnosv1.Cdnos{}).
+		Owns(&corev1.Service{}).
 		Complete(r)
 }
