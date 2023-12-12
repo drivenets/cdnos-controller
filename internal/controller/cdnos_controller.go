18842b3af6b69301b495e12c26220931dad592db
Noam Hadar <nhadar@drivenets.com>
2023-12-12 09:29:40 +0200

    fix

--- internal/controller/cdnos_controller.go
+++ internal/controller/cdnos_controller.go
@@ -44,9 +44,10 @@ type CdnosReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=cdnos.dev.drivenets.net,resources=cdnos,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=cdnos.dev.drivenets.net,resources=cdnos/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=cdnos.dev.drivenets.net,resources=cdnos/finalizers,verbs=update
+//+kubebuilder:rbac:groups=cdnos.dev.drivenets.net,resources=cdnoss,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=cdnos.dev.drivenets.net,resources=cdnoss/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=cdnos.dev.drivenets.net,resources=cdnoss/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=pods;services;secrets,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
